383509ee5dc8859ff3cea55c5c104937d3fe5e8f
Prashanth  Kethavarapu <prashanthkethavarapu@Prashanths-MacBook-Air.local>
2018-04-06 18:00:26 +0530

    Added all

---
+++ register.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Registration System </title>
+<link rel="stylesheet" href="main.css" type="text/css">
+</head>
+<body>
+
+  <div class="header">
+    <h2> Register </h2>
+</div>
+<div class="input-group">
+  Username:
+  <input type="text" 
+
+</body>
+</html>
